6205f71bd4414805f3e78ecfb91f0d5776eb1cb9
Jiroma <maury0110@hotmail.com>
2020-08-06 13:47:40 -0400

    registro, login y verificacion

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 function registrar() {
     var regEmail = document.getElementById('regEmail').value;
     var regPass = document.getElementById('regPass').value;
+    var regPass2 = document.getElementById('regPass2').value;
 
-    firebase.auth().createUserWithEmailAndPassword(regEmail, regPass)
+    if(regPass==regPass2){
+      firebase.auth().createUserWithEmailAndPassword(regEmail, regPass)
+    .then(function(){
+      verificar();
+      location.href="index.html";
+    })
     .catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
         // ...
       });
+    }
+    else{
+      alert('Las contraseñas no coinciden');
+    }    
 }
 function login(){
     var logEmail = document.getElementById('logEmail').value;
@@ -16,11 +26,21 @@ function login(){
 
 
     firebase.auth().signInWithEmailAndPassword(logEmail, logPass)
+    .then(function(){
+      firebase.auth().onAuthStateChanged(function(user){
+        if(user.emailVerified){
+          location.href="main.html";
+        }
+        else{
+          alert('Revise su correo para verificar');
+        }
+      })
+    })
     .catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
-        alert(errorMessage);
+        alert('Correo o contraseña incorrecta');
         // ...
       });
 }
@@ -32,6 +52,11 @@ function observador(){
           console.log('existe usuario activo');
           var displayName = user.displayName;
           var email = user.email;
+
+          console.log('******************');
+          console.log(user.emailVerified);
+          console.log('******************');
+
           var emailVerified = user.emailVerified;
           var photoURL = user.photoURL;
           var isAnonymous = user.isAnonymous;
@@ -46,3 +71,25 @@ function observador(){
       });email-password.html
 }
 observador();
+
+function cerrar() {
+  firebase.auth().signOut()
+  .then(function() {
+    console.log('Saliendo...');
+  })
+  .catch(function(error){
+    console.log(error);
+  })
+
+}
+
+function verificar() {
+  var user = firebase.auth().currentUser;
+user.sendEmailVerification().then(function() {
+  // Email sent.
+  console.log('Enviando correo...');
+}).catch(function(error) {
+  // An error happened.}
+  console.log(error);
+});
+}
