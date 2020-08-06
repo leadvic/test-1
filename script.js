2643040ae8302c70b0c3ab21383286a32ffd8c64
Víctor Díaz Cortés <vmdcortes@gmail.com>
2020-08-06 13:49:59 -0400

    navegation bar added

--- script.js
+++ script.js
@@ -1,48 +1,50 @@
 function registrar() {
-    var regEmail = document.getElementById('regEmail').value;
-    var regPass = document.getElementById('regPass').value;
+  var regEmail = document.getElementById('regEmail').value;
+  var regPass = document.getElementById('regPass').value;
 
-    firebase.auth().createUserWithEmailAndPassword(regEmail, regPass)
+  firebase.auth().createUserWithEmailAndPassword(regEmail, regPass)
     .catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-      });
-}
-function login(){
-    var logEmail = document.getElementById('logEmail').value;
-    var logPass = document.getElementById('logPass').value;
-
-
-    firebase.auth().signInWithEmailAndPassword(logEmail, logPass)
-    .catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        alert(errorMessage);
-        // ...
-      });
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+    });
 }
 
-function observador(){
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-          // User is signed in.
-          console.log('existe usuario activo');
-          var displayName = user.displayName;
-          var email = user.email;
-          var emailVerified = user.emailVerified;
-          var photoURL = user.photoURL;
-          var isAnonymous = user.isAnonymous;
-          var uid = user.uid;
-          var providerData = user.providerData;
-          // ...
-        } else {
-          // User is signed out.
-          console.log('no existe usuario activo');
-          // ...
-        }
-      });email-password.html
+function login() {
+  var logEmail = document.getElementById('logEmail').value;
+  var logPass = document.getElementById('logPass').value;
+
+
+  firebase.auth().signInWithEmailAndPassword(logEmail, logPass)
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(errorMessage);
+      // ...
+    });
+}
+
+function observador() {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      console.log('existe usuario activo');
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      // ...
+    } else {
+      // User is signed out.
+      console.log('no existe usuario activo');
+      // ...
+    }
+  });
+  email - password.html
 }
 observador();
