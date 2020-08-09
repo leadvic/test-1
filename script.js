3ba5dfb93c915d34c45f9ca7040a2d47cee5da28
Víctor Díaz Cortés <vmdcortes@gmail.com>
2020-08-09 15:38:54 -0400

    yandex verification

--- script.js
+++ script.js
@@ -1,111 +1,95 @@
 function registrar() {
-    var regEmail = document.getElementById('regEmail').value;
-    var regPass = document.getElementById('regPass').value;
-    var regPass2 = document.getElementById('regPass2').value;
+  var regEmail = document.getElementById('regEmail').value;
+  var regPass = document.getElementById('regPass').value;
+  var regPass2 = document.getElementById('regPass2').value;
 
-    if(regPass==regPass2){
-      firebase.auth().createUserWithEmailAndPassword(regEmail, regPass)
-    .then(function(){
-      verificar();
-      location.href="index.html";
-    })
-    .catch(function(error) {
+  if (regPass == regPass2) {
+    firebase.auth().createUserWithEmailAndPassword(regEmail, regPass)
+      .then(function() {
+        verificar();
+        location.href = "index.html";
+      })
+      .catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
         // ...
       });
-    }
-    else{
-      alert('Las contraseñas no coinciden');
-    }
+  } else {
+    alert('Las contraseñas no coinciden');
+  }
 }
 
 function login() {
   var logEmail = document.getElementById('logEmail').value;
   var logPass = document.getElementById('logPass').value;
 
-    firebase.auth().signInWithEmailAndPassword(logEmail, logPass)
-    .then(function(){
-      firebase.auth().onAuthStateChanged(function(user){
-        if(user.emailVerified){
-          location.href="main.html";
-        }
-        else{
+  firebase.auth().signInWithEmailAndPassword(logEmail, logPass)
+    .then(function() {
+      firebase.auth().onAuthStateChanged(function(user) {
+        if (user.emailVerified) {
+          location.href = "main.html";
+        } else {
           alert('Revise su correo para verificar');
         }
       })
     })
     .catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        alert('Correo o contraseña incorrecta');
-        // ...
-      });
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert('Correo o contraseña incorrecta');
+      // ...
+    });
 }
 
-function observador(){
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-          // User is signed in.
-          console.log('existe usuario activo');
-          var displayName = user.displayName;
-          var email = user.email;
+function observador() {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      console.log('existe usuario activo');
+      var displayName = user.displayName;
+      var email = user.email;
 
-          console.log('******************');
-          console.log(user.emailVerified);
-          console.log('******************');
+      console.log('******************');
+      console.log(user.emailVerified);
+      console.log('******************');
 
-          var emailVerified = user.emailVerified;
-          var photoURL = user.photoURL;
-          var isAnonymous = user.isAnonymous;
-          var uid = user.uid;
-          var providerData = user.providerData;
-          // ...
-        } else {
-          // User is signed out.
-          console.log('no existe usuario activo');
-          // ...
-        }
-      });email-password.html
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      // ...
+    } else {
+      // User is signed out.
+      console.log('no existe usuario activo');
+      // ...
+    }
+  });
+  email - password.html
 }
 observador();
 
 function cerrar() {
   firebase.auth().signOut()
-  .then(function() {
-    console.log('Saliendo...');
-    location.href='index.html';
-  })
-  .catch(function(error){
-    console.log(error);
-  })
+    .then(function() {
+      console.log('Saliendo...');
+      location.href = 'index.html';
+    })
+    .catch(function(error) {
+      console.log(error);
+    })
 
 }
 
 function verificar() {
   var user = firebase.auth().currentUser;
-user.sendEmailVerification().then(function() {
-  // Email sent.
-  console.log('Enviando correo...');
-}).catch(function(error) {
-  // An error happened.}
-  console.log(error);
-});
-}
-
-
-// Changin Background Images
-var images = ['https://i.pximg.net/img-original/img/2020/03/03/00/00/07/79855820_p0.jpg','https://picsum.photos/200/300/?random'];
-var index = 0;
-
-function buildImage() {
-    document.getElementById('content').style.backgroundImage = 'url('+images[index]+')';
-}
-
-function changeImage() {
-    index++;
-    if (index >= images.length) index = 0;
-        document.getElementById('content').style.backgroundImage = 'url(' + images[index] + ')';
+  user.sendEmailVerification().then(function() {
+    // Email sent.
+    console.log('Enviando correo...');
+  }).catch(function(error) {
+    // An error happened.}
+    console.log(error);
+  });
 }
