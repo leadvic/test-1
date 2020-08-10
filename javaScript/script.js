10ac367075d4ba442f52e932958f725dc21e2ed0
Víctor Díaz Cortés <vmdcortes@gmail.com>
2020-08-10 10:25:09 -0400

    developing the dashboard

--- javaScript/script.js
+++ javaScript/script.js
@@ -93,3 +93,19 @@ function verificar() {
     console.log(error);
   });
 }
+
+// Upload a File to FireBase
+
+var img = document.getElementById('img');
+
+let file = {};
+
+function chooseFile(e) {
+  file = e.target.files[0];
+}
+
+function uploadFile() {
+  firebase.storage().ref('users/' + auth.user.uid + '/profile.jpg').put(file).then(function () {
+    console.log('uploaded succesfully')
+  })
+}
